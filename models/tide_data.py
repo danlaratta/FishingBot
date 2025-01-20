013bab5e1ba0308a31349863d7a96529aa0eed9d
danlaratta <dan.laratta.jr@gmail.com>
2025-01-20 17:53:40 -0500

    MVP Complete
    Completed the MVP for this project, can add more features in the future.

--- models/tide_data.py
+++
@@ -1,9 +0,0 @@
-
-class TideData:
-    def __init__(self, high_tide_time_am, high_tide_time_pm, low_tide_time_am, low_tide_time_pm):
-        self.high_tide_time_am = high_tide_time_am
-        self.high_tide_time_pm = high_tide_time_pm
-        self.low_tide_time_am = low_tide_time_am
-        self.low_tide_time_pm = low_tide_time_pm
-        # self.water_temp = water_temp
-        # self.wave_height = wave_height
